66ab281647ea987c8297816a393e640ec1954f5c
Casey <caclowers@gmail.com>
2018-11-30 17:07:46 -0600

    added Header component which contains Hamburger

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+
 import logo from './woodland_logos_cropped.jpg';
+
+import Hamburger from './src/components/Header/Header.js';
 
 export default class App extends React.Component {
   render() {
@@ -8,17 +11,21 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <View className="App-header" style={styles.header}>
           <Image 
-          source={logo} 
-          className="App-logo" 
-          alt="logo"
-          style={{width: 193, height: 161}}/>
-            <Text href="something here">Home</Text>
-            <Text href="something here">Pizza</Text>
-            <Text href="something here">Sandwiches</Text>
-            <Text href="something here">Reviews</Text>
-            <Text href="something here">About</Text>
-            <Text href="something here">Directions</Text>
+            source={logo} 
+            className="App-logo" 
+            alt="logo"
+            style={{width: 193, height: 161}}/>
+          <Hamburger />
+        </View >
+        <View style={styles.content}>
+          <Text href="something here">Home</Text>
+          <Text href="something here">Pizza</Text>
+          <Text href="something here">Sandwiches</Text>
+          <Text href="something here">Reviews</Text>
+          <Text href="something here">About</Text>
+          <Text href="something here">Directions</Text>
         </View>
+          
       </View>
     );
   }
@@ -27,13 +34,20 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#B4DA55',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'flex-start',
     marginTop: 48,
   },
   header:{
     flex: 1,
-    marginTop:24
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginTop:48,
   },
+  content:{
+    flex: 3,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  }
 });
